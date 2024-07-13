df04f3cc8288b70cadbf1116b0c2ffca456431c0
Promise Nmor <nmorchibogu98@gmail.com>
2024-07-13 23:28:45 +0100

    added vercel.json

--- crmApp/settings.py
+++ crmApp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-d21fi75^2xqs+^pyf&w6zno(&1--$knuhdha#7)ei78c^tc5uo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
