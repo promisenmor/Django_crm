6ba8857602c6aeb207696c51c2052a41ecc8b1f0
Promise Nmor <nmorchibogu98@gmail.com>
2024-07-20 01:07:25 +0100

    changed settings in allowed host

--- crmApp/settings.py
+++ crmApp/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'crmApp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['.vercel.app'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
